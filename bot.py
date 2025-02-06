79e381adaa941dc3ea9cfe605bec4724b0e5a072
Neeraj Patel <neerajnpatel18@gmail.com>
2025-02-06 16:37:54 +0530

    init stuff

---
+++ bot.py
@@ -0,0 +1,19 @@
+# -------- Select grid size --------
+
+
+# -------- Select featured image --------
+
+
+
+# -------- Select remaining images --------
+
+# ------ Ensure featured image is not in remaining images ------
+
+# -------- Resize images to be 1 by 1 provide GUI menu for what to crop if not 1 by 1 already --------
+
+
+# -------- Select gap size between images --------
+
+
+# -------- Export jpg of final grid of 1080 × 1920 pixels --------
+
